d374bf15f02ea881436f2e18291621b033107be6
kaelll2 <lacabamichael60@gmail.com>
2025-03-11 14:16:16 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using OOP;
+
+class MainProgram
+{
+    public static void Main()
+    {
+        OOP.ManageProduct.InsertNewProduct newProduct = new OOP.ManageProduct.InsertNewProduct();
+        string productname;
+        string productdescription;
+        int productprice;
+        int limit = 0;
+        int setter;
+
+
+        Console.WriteLine(@"
+                                           
+ =======================================================================================================================
+
+
+
+                       / __ \_________  ____/ /_  _______/ /_   /  _/___  ________  _____/ /_(_)___  ____ 
+                      / /_/ / ___/ __ \/ __  / / / / ___/ __/   / // __ \/ ___/ _ \/ ___/ __/ / __ \/ __ \
+                     / ____/ /  / /_/ / /_/ / /_/ / /__/ /_   _/ // / / (__  )  __/ /  / /_/ / /_/ / / / /
+                    /_/   /_/   \____/\__,_/\__,_/\___/\__/  /___/_/ /_/____/\___/_/   \__/_/\____/_/ /_/ 
+
+
+
+ =======================================================================================================================");
+
+        Console.Write(@"
+ 
+
+                                                 | Enter insertion limit: |
+
+                                                         - ");
+
+        while (!int.TryParse(Console.ReadLine(), out setter) || setter < 0)
+        {
+
+            Console.Write(@"
+                
+                                               | Please input a valid range: |
+
+                                                         - ");
+        }
+        Console.Clear();
+
+
+        do
+        {
+
+
+            do
+            {
+                product.lineBreaker.lineBreak();
+                Console.Clear();
+                Console.Write(@"
+______  ___________  _______________ _   ________  __  ___ _   ______  
+| __| \||_ _| __| . \ | . | . |   |  \|| \| __|_ _\ | \||  \|\/\| __|_] 
+|  ]|  \| |||  ]|  <_ | __|  <| . | . ||_|| \__ ||  |  \| . |   |  ],-. 
+|___|/\_/ |/|___|/\_/ |/  |/\_|___|___|___|___/ |/  |/\_|/\_|/v\|___'-'
+
+ - ");
+                productname = Console.ReadLine();
+
+            } while (string.IsNullOrEmpty(productname));
+
+            product.lineBreaker.lineBreak();
+            Console.Write(@"
+______  ____________  _______________ _   ________  ______________________   
+| __| \||_ _| __| . \ | . | . |   |  \|| \| __|_ _\ | . | . |___| __| __|_]  
+|  ]|  \| |||  ]|  <_ | __|  <| . | . ||_|| \__ ||  | __|  <| / | \_|  ],-.  
+|___|/\_/ |/|___|/\_/ |/  |/\_|___|___|___|___/ |/  |/  |/\_|/  |___|___'-'  
+
+ - ");
+
+            while (!int.TryParse(Console.ReadLine(), out productprice) || productprice < 0)
+            {
+                product.lineBreaker.lineBreak();
+                Console.Write(@"
+                                        | Invalid input. Please enter a valid price: ");
+            }
+
+
+            do
+            {
+                product.lineBreaker.lineBreak();
+                Console.Write(@"
+______  ____________  _______________ _   ________  ___ _______ ______________________________  __  
+| __| \||_ _| __| . \ | . | . |   |  \|| \| __|_ _\ |  \| __| _\| __| . |___| . |_ _|___|   | \||_] 
+|  ]|  \| |||  ]|  <_ | __|  <| . | . ||_|| \__ ||  | . |  ][__ | \_|  <| / | __/ ||| / | . |  \,-. 
+|___|/\_/ |/|___|/\_/ |/  |/\_|___|___|___|___/ |/  |___|___|___|___|/\_|/  |/    |/|/  |___|/\_'-' 
+
+ - ");
+                productdescription = Console.ReadLine();
+            } while (string.IsNullOrEmpty(productdescription));
+
+            Console.Clear();
+
+
+            newProduct.InsertData(productname, productprice, productdescription);
+
+
+            limit++;
+
+
+        } while (limit < setter);
+
+
+
+        Console.ReadKey();
+    }
+}
---
+++ insertNewProduct.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace OOP
+{
+    class ManageProduct
+    {
+        public class InsertNewProduct
+        {
+            private string server = "localhost";
+            private string database = "itelect";
+            private string username = "root";
+            private string password = "";
+            private string connString;
+
+            public InsertNewProduct()
+            {
+                connString = $"Server={server};Database={database};User ID={username};Password={password};";
+            }
+            
+            public void InsertData(string productName, int productPrice, string productDescription)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        product.lineBreaker.lineBreak();
+                        Console.WriteLine(@"
+                                    
+                                 __                                    __ __      
+                                /   _  _  _  _ _|_ _ _|  |_ _   |\/|  (_ /  \|  | 
+                                \__(_)| )| )(-(_|_(-(_|  |_(_)  |  |\/__)\_\/|__.");
+
+
+
+                        string query = "INSERT INTO productlist (productName, productPrice, productDescription) VALUES (@name, @price, @description)";
+
+                        using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@name", productName);
+                            cmd.Parameters.AddWithValue("@price", productPrice);
+                            cmd.Parameters.AddWithValue("@description", productDescription);
+
+                            int rowsAffected = cmd.ExecuteNonQuery();
+
+                            Console.WriteLine(rowsAffected > 0 ? @"
+
+
+
+=======================================================================================================================
+ 
+            |   \ __ _| |_ __ _  (_)_ _  ______ _ _| |_ ___ __| |  ____  _ __ __ ___ ______/ _|_  _| | |_  _| |
+            | |) / _` |  _/ _` | | | ' \(_-/ -_| '_|  _/ -_/ _` | (_-| || / _/ _/ -_(_-(_-|  _| || | | | || |_|
+            |___/\__,_|\__\__,_| |_|_||_/__\___|_|  \__\___\__,_| /__/\_,_\__\__\___/__/__|_|  \_,_|_|_|\_, (_)
+                                                                                                        |__/
+=======================================================================================================================" : @"
+
+                        ___     _ _        _   _         _                 _        _      _        _ 
+                        | ____ _(_| |___ __| | | |_ ___  (_)_ _  ______ _ _| |_   __| |__ _| |_ __ _| |
+                        | _/ _` | | / -_/ _` | |  _/ _ \ | | ' \(_-/ -_| '_|  _| / _` / _` |  _/ _` |_|
+                        |_|\__,_|_|_\___\__,_|  \__\___/ |_|_||_/__\___|_|  \__| \__,_\__,_|\__\__,_(_)");
+
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
